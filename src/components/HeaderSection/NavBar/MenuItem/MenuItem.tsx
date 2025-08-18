a28e9dc89da1736c2ee7c0ac806d497a97da22e2
SamanPrasad <samanseedr2@gmail.com>
2025-08-18 23:14:02 +0530

    menu finished

---
+++ src/components/HeaderSection/NavBar/MenuItem/MenuItem.tsx
@@ -0,0 +1,69 @@
+import { useEffect, useRef, useState } from "react";
+import { IoIosArrowForward } from "react-icons/io";
+import { Link, useLocation } from "react-router";
+import "./MenuItem.css";
+
+interface Menu {
+  id: number;
+  menu: string;
+  level: number;
+  uri: string;
+  children?: Menu[];
+}
+
+function MenuItem({ menu }: { menu: Menu }) {
+  const location = useLocation();
+  const divRef = useRef<HTMLDivElement | null>(null);
+  const [overflow, setOverflow] = useState(false);
+  const [height, setHeight] = useState(0);
+  const [open, setOpenStatus] = useState(false);
+
+  useEffect(() => {
+    setHeight(divRef.current?.scrollHeight ?? 0);
+  }, [open]);
+
+  return (
+    <div
+      className={"menu-item group z-50 flex justify-start relative"}
+      onMouseEnter={() => setOpenStatus(true)}
+      onMouseLeave={() => {
+        setOpenStatus(false);
+        setOverflow(false);
+      }}
+    >
+      <Link
+        to={menu.uri}
+        className={`flex justify-between items-center w-full px-9 text-start py-1 z-40 ${
+          location.pathname + location.search == menu.uri ? "active" : ""
+        }`}
+      >
+        {menu.menu}
+        {menu.children && <IoIosArrowForward className="ms-2 mt-[2px]" />}
+      </Link>
+      {open && menu.children && (
+        <div
+          className={`menu-item-children rounded-md 
+            ${open ? " menu-open" : ""}
+            ${menu.level < 2 ? " parent-level" : " sub-level"}
+          `}
+          style={{
+            height,
+            opacity: open ? 1 : 0,
+            transform: open ? "translateY(0)" : "translateY(-20px)",
+            overflow: overflow ? "visible" : "hidden",
+          }}
+          ref={divRef}
+          onTransitionEnd={() => {
+            setOverflow(true);
+          }}
+        >
+          {menu.children.map((item) => (
+            <MenuItem menu={item} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MenuItem;
--- src/components/HeaderSection/NavBar/Navbar.tsx
+++ src/components/HeaderSection/NavBar/Navbar.tsx
@@ -1,27 +1,13 @@
-import logoDark from "../../../assets/logo-dark.svg";
-import { Link, NavLink, useLocation } from "react-router";
 import "./NavBar.css";
 import "flag-icons/css/flag-icons.min.css";
-import NavItem from "./NavItem/NavItem";
 import menu from "../../../data/menu";
-import { useRef } from "react";
-import Item from "./Item/Item";
+import MenuItem from "./MenuItem/MenuItem";
 
 function Navbar() {
-  const location = useLocation();
-  const path = location.pathname + location.search;
-  const contextRef = useRef<HTMLDivElement | null>(null);
-
-  const mouseOver = () => {
-    const height = contextRef.current?.offsetHeight;
-    console.log(height);
-    console.log(contextRef.current);
-    // console.log(num);
-  };
   return (
     <div className="navbar text-white bg-black h-12 w-full flex justify-center items-center gap-3 px-40 z-50">
-      {menu.map((item, index) => {
-        return <Item menu={item} />;
+      {menu.map((item) => {
+        return <MenuItem menu={item} />;
       })}
     </div>
   );
